867534f5d6171d019fba8b2084365efc7472bdc0
mceciabate <m.cecilia.abate@gmail.com>
2023-08-02 00:47:45 -0300

    microservicios bases registrandose en Eureka

---
+++ api-vakita/ms-vakitas/src/main/java/com/grupo3/msvakitas/MsVakitasApplication.java
@@ -0,0 +1,13 @@
+package com.grupo3.msvakitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVakitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsVakitasApplication.class, args);
+	}
+
+}
