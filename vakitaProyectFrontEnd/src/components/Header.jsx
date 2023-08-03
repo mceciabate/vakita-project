198c2d85e44d9fcaf03a2faf5d66fa6bc77509af
Stephania-Martheyn <stephaniemartheyn@hotmail.com>
2023-08-02 21:35:12 -0500

    loading terminado y responsive

--- vakitaProyectFrontEnd/src/components/Header.jsx
+++ vakitaProyectFrontEnd/src/components/Header.jsx
@@ -1,29 +1,30 @@
+import React from "react";
 import "../styles/header.css"
 import logoVaca from "../assets/LogoVaca.svg";
 
 
-function Header () {
-    return (
-      <header className= "header">
+function Header() {
+  return (
+    <header className="header">
 
-        <img className="header-logo" src={logoVaca} alt="logo" />
+      <img className="header-logo" src={logoVaca} alt="logo" />
 
 
-        <div className="header-botones">
-       
-          <button className="nav_item" id="Iniciar-sesion">
-            Iniciar sesión
-          </button>
-        
-    
-          <button className="nav_item" id="Crear-cuenta">
-            Crear cuenta
-          </button>
-        
+      <div className="header-botones">
+
+        <button className="nav_item" id="Iniciar-sesion">
+          Iniciar sesión
+        </button>
+
+
+        <button className="nav_item" id="Crear-cuenta">
+          Crear cuenta
+        </button>
+
       </div>
 
-      </header>
-    );
-  }
+    </header>
+  );
+}
 
-  export default Header;
+export default Header;
