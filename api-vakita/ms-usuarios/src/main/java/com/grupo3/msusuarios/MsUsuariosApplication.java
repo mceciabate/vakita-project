997e4d923c2b9ea4099fe7dc59b6f0d72705963a
matiasRaspa <maatiii.r@gmail.com>
2023-08-04 19:02:22 -0300

    [NEW] globalExeption ms-usuarios

--- api-vakita/ms-usuarios/src/main/java/com/grupo3/msusuarios/MsUsuariosApplication.java
+++ api-vakita/ms-usuarios/src/main/java/com/grupo3/msusuarios/MsUsuariosApplication.java
@@ -1,5 +1,6 @@
 package com.grupo3.msusuarios;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,6 +10,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class MsUsuariosApplication {
 
 	public static void main(String[] args) {
+
+		//log4j configuration for global errors
+		PropertyConfigurator.configure("log4j.properties");
+
 		SpringApplication.run(MsUsuariosApplication.class, args);
 	}
 
