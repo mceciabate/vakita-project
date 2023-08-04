f7e6064f93c0565ab5cf136b2ca69446d07621ab
matiasRaspa <maatiii.r@gmail.com>
2023-08-04 16:30:33 -0300

    [UPDATE] Correccion h2 properties

--- api-vakita/ms-usuarios/src/test/java/com/grupo3/msusuarios/service/UserServiceTest.java
+++ api-vakita/ms-usuarios/src/test/java/com/grupo3/msusuarios/service/UserServiceTest.java
@@ -5,11 +5,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserServiceTest {
 
     @Autowired
