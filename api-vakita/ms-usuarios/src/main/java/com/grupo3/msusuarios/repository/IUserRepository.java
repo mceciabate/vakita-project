9e5b922e969406ff65a44a32b5a7fdc32055709f
Cecilia Abate <m.cecilia.abate@gmail.com>
2023-08-06 00:11:01 +0000

    Revert "Merge branch 'vakitascript' into 'main'"
    This reverts merge request !9

--- api-vakita/ms-usuarios/src/main/java/com/grupo3/msusuarios/repository/IUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.grupo3.msusuarios.repository;
-
-import com.grupo3.msusuarios.model.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IUserRepository extends JpaRepository<User, Long>{
-}
--- api-vakita/ms-vakitas/src/main/java/com/grupo3/msvakitas/repository/IVakitaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.grupo3.msvakitas.repository;
-
-import com.grupo3.msvakitas.model.entity.Vakita;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IVakitaRepository extends JpaRepository<Vakita, Long> {
-}
