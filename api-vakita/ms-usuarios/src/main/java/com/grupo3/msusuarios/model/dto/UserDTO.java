199cb6df1ceb18fc50e026806f6d55794137cee9
mceciabate <m.cecilia.abate@gmail.com>
2023-08-08 09:47:08 -0300

    data set

--- api-vakita/ms-usuarios/src/main/java/com/grupo3/msusuarios/model/dto/UserDTO.java
+++ api-vakita/ms-usuarios/src/main/java/com/grupo3/msusuarios/model/dto/UserDTO.java
@@ -1,11 +1,13 @@
 package com.grupo3.msusuarios.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO {
 
