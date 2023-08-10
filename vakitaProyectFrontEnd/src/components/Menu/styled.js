eaf3de2b77ac36f783f6c8e19de81686ff0dfd28
Cindy Suarez <cindyailensuarez@gmail.com>
2023-08-10 11:12:46 -0300

    acomodo estilos de menu hamburguesa

--- vakitaProyectFrontEnd/src/components/Menu/styled.js
+++ vakitaProyectFrontEnd/src/components/Menu/styled.js
@@ -1,6 +1,7 @@
 import { styled } from "styled-components";
 
 export const MenuLateral = styled.section`
+display: none;
     width: 18rem;
     padding: 2.5rem 1rem;
     height: calc(100vh - 112px);
@@ -13,8 +14,12 @@ export const MenuLateral = styled.section`
 
     /* Aqui se establece que el boton salir no se mostrara en desktop*/
     @media (min-width: 768px){
-
-        padding: 2.5rem 1rem;
+display:flex;
+width: 18rem;
+    padding: 2.5rem 1rem;
+    height: calc(100vh - 112px);
+    font-family: 'Inria Sans', sans-serif;
+       
         .botonSalir {
             display: block;
         flex-direction: row;
@@ -39,6 +44,7 @@ export const HeaderMenuLateral = styled.div`
     color:white;
     padding: 20px;
     img.avatar {
+    
         width:40%;
         height:100%;
         margin-right:20px;
@@ -85,7 +91,7 @@ export const BloqueOptions = styled.div`
 
 export const MenuDiv=styled.div`
 
-    display: none;
+    display: flex;
 
     @media (min-width: 769px){
           
