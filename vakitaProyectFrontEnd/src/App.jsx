2c6e4d6555e3c76b152c4c54e9124aa4240d979f
DayanaOtagri <heidyotagri@hotmail.com>
2023-08-10 17:56:16 -0500

    Cambios y modulos

--- vakitaProyectFrontEnd/src/App.jsx
+++ vakitaProyectFrontEnd/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Routers from './Routes/Routers';
 import Header from './components/Header.jsx';
-import Login from './components/Login/Login';
+
 
 
 function App() {
@@ -10,9 +10,7 @@ function App() {
     <div className="App"> 
     <Header/>
   
-      <Routers/>
-      <Login/>
-      
+      <Routers/>      
   
     </div>
   )
