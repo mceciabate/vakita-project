3524f1cd359e70619584aa360be851f9dba14fa6
mceciabate <m.cecilia.abate@gmail.com>
2023-08-14 17:15:22 -0300

    state

--- api-vakita/configServer/src/main/java/com/dh/configserver/ConfigServerApplication.java
+++ api-vakita/configServer/src/main/java/com/dh/configserver/ConfigServerApplication.java
@@ -6,13 +6,23 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
+
 @SpringBootApplication
 @EnableConfigServer
 @RefreshScope
 public class ConfigServerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		SpringApplication.run(ConfigServerApplication.class, args);
+
 	}
+
+
+
 
 }
