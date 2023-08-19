599ab6bb5ec290e338988b9cfc60317d8ab227f8
mceciabate <m.cecilia.abate@gmail.com>
2023-08-18 23:49:50 -0300

    actualziado a version de vaciar vakita

--- api-vakita/ms-usuarios/src/main/resources/data.sql
+++ api-vakita/ms-usuarios/src/main/resources/data.sql
@@ -1,16 +1,13 @@
---CREATE SCHEMA IF NOT EXISTS `usersdb` ;
---USE `usersdb`;
-
-
-insert into 0723TDPI2N1C01LAED1021FT_GRUPO3.users values( default, '1995-01-17',  '22222222', 'mglendinning0@correo.com',  'Menon', 'Maria', '4321');
-insert into 0723TDPI2N1C01LAED1021FT_GRUPO3.users values( default, '2001-01-17',  '33333333', 'rrmaria@micorreo.com','Santiago', 'Maria', '6789');
-insert into 0723TDPI2N1C01LAED1021FT_GRUPO3.users values(default, '1994-01-17',  '44444444', 'andreagal@micorreo.com',  'Escalante', 'Andrea', '9876');
-insert into 0723TDPI2N1C01LAED1021FT_GRUPO3.users values(default, '1981-01-17','55555555', 'melodaniel@micorreo.com', 'Perez', 'Daniel', '3698');
-insert into 0723TDPI2N1C01LAED1021FT_GRUPO3.users values(default, '2002-01-17', '6666666', 'karenv@micorreo.com', 'Moreno', 'Karen', '0101');
-insert into 0723TDPI2N1C01LAED1021FT_GRUPO3.users values(default, '2004-10-18', '7777777', 'josemartinez@micorreo.com','Moreno', 'Jose', '7412');
-insert into 0723TDPI2N1C01LAED1021FT_GRUPO3.users values(default, '2001-12-19','8888888', 'fergarcia@micorreo.com','Garcia', 'Fernando', '7412');
-insert into 0723TDPI2N1C01LAED1021FT_GRUPO3.users values( default, '2003-04-13', '9999999', 'luciamesa@micorreo.com', 'Mesa', 'Lucía',  '7412');
-insert into 0723TDPI2N1C01LAED1021FT_GRUPO3.users values( default,'1987-11-11',  '10101010','diazjuan@micorreo.com', 'Diaz', 'Juan', '7412');
+select * from users;
+insert into 0723TDPI2N1C01LAED1021FT_GRUPO3.users values( default, 0.0, '1995-01-17','22222222', 'mglendinning0@correo.com',  'Menon', 'Maria', '4321');
+insert into 0723TDPI2N1C01LAED1021FT_GRUPO3.users values( default, 0.0, '2001-01-17','33333333', 'rrmaria@micorreo.com','Santiago', 'Maria', '6789');
+insert into 0723TDPI2N1C01LAED1021FT_GRUPO3.users values( default, 0.0, '1994-01-17','44444444', 'andreagal@micorreo.com',  'Escalante', 'Andrea', '9876');
+insert into 0723TDPI2N1C01LAED1021FT_GRUPO3.users values( default, 0.0, '1981-01-17', '55555555', 'melodaniel@micorreo.com', 'Perez', 'Daniel', '3698');
+insert into 0723TDPI2N1C01LAED1021FT_GRUPO3.users values( default, 0.0, '2002-01-17','6666666', 'karenv@micorreo.com', 'Moreno', 'Karen', '0101');
+insert into 0723TDPI2N1C01LAED1021FT_GRUPO3.users values( default, 0.0, '2004-10-18','7777777', 'josemartinez@micorreo.com','Moreno', 'Jose', '7412');
+insert into 0723TDPI2N1C01LAED1021FT_GRUPO3.users values( default, 0.0, '2001-12-19','8888888', 'fergarcia@micorreo.com','Garcia', 'Fernando', '7412');
+insert into 0723TDPI2N1C01LAED1021FT_GRUPO3.users values( default, 0.0, '2003-04-13','9999999', 'luciamesa@micorreo.com', 'Mesa', 'Lucía',  '7412');
+insert into 0723TDPI2N1C01LAED1021FT_GRUPO3.users values( default, 0.0, '1987-11-11','10101010','diazjuan@micorreo.com', 'Diaz', 'Juan', '7412');
 
 
 
