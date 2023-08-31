51ac5534e18f94e6ff47d02becbf2bdd98367e93
matiasRaspa <maatiii.r@gmail.com>
2023-08-31 11:33:11 -0300

    [UPDATE] validaciones dto + email facelift

--- api-vakita/ms-usuarios/src/main/java/com/grupo3/msusuarios/model/dto/UserDTO.java
+++ api-vakita/ms-usuarios/src/main/java/com/grupo3/msusuarios/model/dto/UserDTO.java
@@ -1,5 +1,8 @@
 package com.grupo3.msusuarios.model.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,11 +15,19 @@ import java.time.LocalDate;
 public class UserDTO {
 
     private Long id;
+    @NotBlank(message = "No puede ingresar un nombre vacio")
+    @Size(max = 30 ,message = "El nombre no puede contener mas de 30 caracteres")
     private String name;
+    @NotBlank(message = "No puede ingresar un apellido vacio")
+    @Size(max = 30 ,message = "El apellido no puede contener mas de 30 caracteres")
     private String lastName;
+    @NotBlank(message = "No puede ingresar un dni vacio")
     private String dni;
+    @NotBlank(message = "No puede ingresar un email vacio o email existente")
     private String email;
+    @NotBlank(message = "No puede ingresar un password vacio")
     private String password;
+    @NotNull(message = "No puede ingresar una fecha vacia")
     private LocalDate birthdate;
     private Double account_balance = 0.0;
 }
--- api-vakita/ms-usuarios/src/main/java/com/grupo3/msusuarios/service/impl/EmailService.java
+++ api-vakita/ms-usuarios/src/main/java/com/grupo3/msusuarios/service/impl/EmailService.java
@@ -1,8 +1,10 @@
 package com.grupo3.msusuarios.service.impl;
 
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,13 +18,25 @@ public class EmailService {
     }
 
     public void sendEmail(String to, String subject, String text) throws Exception {
+//        try {
+//            SimpleMailMessage message = new SimpleMailMessage();
+//            message.setTo(to);
+//            message.setSubject(subject);
+//            message.setText(text);
+//            javaMailSender.send(message);
+//        }catch (Exception e){
+//            throw new Exception(e.getMessage());
+//        }
         try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(to);
-            message.setSubject(subject);
-            message.setText(text);
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text, true); // Indicamos que el contenido es HTML
+
             javaMailSender.send(message);
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new Exception(e.getMessage());
         }
     }
