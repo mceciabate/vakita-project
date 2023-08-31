881dd13ed8761d22085faf039148c62e77f4077a
mceciabate <m.cecilia.abate@gmail.com>
2023-08-31 12:05:55 -0300

    ms-transactions

---
+++ api-vakita/ms-transactions/src/test/java/grupo3/mstransactions/MsTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo3.mstransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
