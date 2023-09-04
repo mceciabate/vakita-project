1bb232bd6a28c148a914b1d610d484ad27995eb4
matiasRaspa <maatiii.r@gmail.com>
2023-09-04 00:42:42 -0300

    unir ms-usuarios a backend

--- api-vakita/ms-vakitas/src/main/java/com/grupo3/msvakitas/repository/ITransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.grupo3.msvakitas.repository;
-
-import com.grupo3.msvakitas.model.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ITransactionRepository extends JpaRepository<Transaction, Long> {
-}
--- api-vakita/ms-vakitas/src/test/java/com/grupo3/msvakitas/MsVakitasApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.grupo3.msvakitas;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MsVakitasApplicationTests {
-
-
-}
