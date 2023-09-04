0e30264cac270b11b4d4e89e6e1cf1d71dd4f642
Cindy Suarez <cindyailensuarez@gmail.com>
2023-09-04 20:46:27 -0300

    merge perfil

--- vakitaProyectFrontEnd/src/pages/myVakita/MyVakita.jsx
+++ vakitaProyectFrontEnd/src/pages/myVakita/MyVakita.jsx
@@ -27,6 +27,7 @@ const MyVakita = () => {
             headers: {
               "Content-type": "application/json",
               Authorization: `Bearer ${token}`,
+              
             },
           });
       
