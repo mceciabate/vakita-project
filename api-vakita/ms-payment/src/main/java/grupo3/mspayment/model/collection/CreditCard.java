ef576a8c89918f7f7a7db48dc868bc6db0fd25c4
mceciabate <m.cecilia.abate@gmail.com>
2023-09-08 16:23:56 -0300

    validaciones ready

--- api-vakita/ms-payment/src/main/java/grupo3/mspayment/model/collection/CreditCard.java
+++ api-vakita/ms-payment/src/main/java/grupo3/mspayment/model/collection/CreditCard.java
@@ -7,9 +7,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.io.Serial;
-import java.io.Serializable;
 import java.time.LocalDate;
 
 @Data
