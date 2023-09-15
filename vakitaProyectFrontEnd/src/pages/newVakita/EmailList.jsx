9b63440972e8d3c5ef53d268e7cdf01c7513d6d4
Stephania-Martheyn <stephaniemartheyn@hotmail.com>
2023-09-15 17:57:22 -0500

    Cambios input de agregar integrantes

--- vakitaProyectFrontEnd/src/pages/newVakita/EmailList.jsx
+++ vakitaProyectFrontEnd/src/pages/newVakita/EmailList.jsx
@@ -7,11 +7,11 @@ const EmailList = ({ emails, onRemove }) => {
   return (
     <div className="boxItems">
       <h4>¿Quiénes van a ser los integrantes de esta vaka?</h4>
-      <div>
+      <div className="contenedor-miembros">
         {emails.length > 0 && emails.map((member, index) => (
           <div key={index} className='memberAdded'>
             <div className='userIcon'>
-              <FontAwesomeIcon icon={faUser} />
+              <FontAwesomeIcon icon={faUser}  />
             </div>
             {member}
             <button type="button" className="buttonDeleted" onClick={() => onRemove(index)}>
