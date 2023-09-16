7711547419d5497138a969be3a36120172e5c201
EFFM00 <fischietto.elena@gmail.com>
2023-09-16 09:39:27 -0300

    maquetacion formulario sin estilos

--- vakitaProyectFrontEnd/src/pages/withdrawal/WithdrawalMoney.jsx
+++ vakitaProyectFrontEnd/src/pages/withdrawal/WithdrawalMoney.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useFormik } from "formik";
 import axios from 'axios';
 import '../../styles/withdrawalPage.css';
 
@@ -7,11 +8,10 @@ const WithdrawalMoney = () => {
   const [accountBalance, setAccountBalance] = useState(0);
 
   useEffect(() => {
-
     axios
       .get(`http://107.22.65.36:8080/api/v1/usuarios/${userId}`)
       .then((response) => {
-   
+
         const user = response.data;
         setAccountBalance(user.account_balance);
       })
@@ -31,19 +31,71 @@ const WithdrawalMoney = () => {
 
   return (
     <>
-      <div className='cointainerW'>
-        <div className='boxW'>
-          <div className='boxTextW'>
-            <div>
-            <p className='text1W'>Saldo disponible</p>
-            </div>
-            <div>
-             <p className='text2W'>
-              {formatCurrency(accountBalance)} <span>pesos</span>
-             </p>
+      <div className="cointainerW">
+        <section className="sectionInfoBalance">
+          <div className="boxW">
+            <div className="boxTextW">
+              <div>
+                <p className="text1W">Saldo disponible</p>
+              </div>
+              <div>
+                <p className="text2W">
+                  {formatCurrency(accountBalance)} <span>pesos</span>
+                </p>
+              </div>
             </div>
           </div>
-        </div>
+
+          <div>
+            <h3>Retirar dinero</h3>
+
+            <form>
+              <fieldset>
+                <label htmlFor="amount">Monto a retirar</label>
+                <input
+                  type="number"
+                  name="amount"
+                  id="amount"
+                  // value={}
+                  // onChange={}
+                  // onBlur={}
+                />
+              </fieldset>
+
+              <fieldset>
+                <label htmlFor="acountType">¿Es una cuenta corriente?</label>
+                <label htmlFor="acountType">
+                  <input
+                    type="radio"
+                    name="acountType"
+                    id="acountType"
+                    value={true}
+                    // value={}
+                    // onChange={}
+                    // onBlur={}
+                  />
+                  Sí
+                </label>
+
+                <label htmlFor="acountType">
+                  <input
+                    type="radio"
+                    name="acountType"
+                    id="acountType"
+                    value={false}
+                    // value={}
+                    // onChange={}
+                    // onBlur={}
+                  />
+                  No
+                </label>
+              </fieldset>
+
+              <button type='submit'>Retirar dinero</button>
+
+            </form>
+          </div>
+        </section>
       </div>
     </>
   );
