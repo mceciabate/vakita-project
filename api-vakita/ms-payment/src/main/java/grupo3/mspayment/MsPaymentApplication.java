00e1adb445b33b5459e17bcbc9d2909b75afb18c
mceciabate <m.cecilia.abate@gmail.com>
2023-09-16 15:42:58 -0300

    cambios

--- api-vakita/ms-payment/src/main/java/grupo3/mspayment/MsPaymentApplication.java
+++ api-vakita/ms-payment/src/main/java/grupo3/mspayment/MsPaymentApplication.java
@@ -3,6 +3,8 @@ package grupo3.mspayment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import  org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
