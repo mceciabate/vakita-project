4a27e9e41071f689f1773836c3fe7d35e47f8901
mceciabate <m.cecilia.abate@gmail.com>
2023-09-20 01:08:28 -0300

    avances

--- api-vakita/ms-usuarios/src/main/java/com/grupo3/msusuarios/MsUsuariosApplication.java
+++ api-vakita/ms-usuarios/src/main/java/com/grupo3/msusuarios/MsUsuariosApplication.java
@@ -4,6 +4,9 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication/*(exclude = {SecurityAutoConfiguration.class,
 		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class})*/
@@ -12,6 +15,19 @@ public class MsUsuariosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsUsuariosApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*");
+			}
+		};
 	}
 
 }
